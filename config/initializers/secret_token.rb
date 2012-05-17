377e40589558c9dec6594de84e312ef746da1e34
linkcd <linkcd@gmail.com>
2012-05-17 11:35:23 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeMonitor::Application.config.secret_token = 'ebe63a11b2ddebd07fb81fd3fb29d2f95acb7f145f33e63289c90ae64f9b12a650e8366fa51b16bf6d58fe0150ccbecf1630e9d874d2fcd4e1191af3c81b1ee8'
